b20fb87e984aa36bc321131c42a7420cd82fdf2f
Adam Virostek <adam.virostek@gmail.com>
2023-10-19 00:22:19 +0200

    add createUserInFirestore function

--- src/services/firestore.jsx
+++ src/services/firestore.jsx
@@ -1,12 +1,21 @@
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { getFirestore, getDocs, collection, doc, setDoc, query, where } from "firebase/firestore";
+import app from "./firebase";
 
 const db = getFirestore(app);
 
-const fetchData = async () => {
+export const fetchData = async () => {
   const querySnapshot = await getDocs(collection(db, "users"));
   querySnapshot.forEach((doc) => {
     const dataAsString = JSON.stringify(doc.data(), null, 2);
     console.log(`${doc.id} => ${dataAsString}`);
   });
 };
-fetchData();
+
+export const createUserInFirestore = async (userId, userData) => {
+  const userRef = doc(db, "users", userId);
+  try {
+    await setDoc(userRef, userData);
+  } catch (error) {
+    throw error;
+  }
+};
