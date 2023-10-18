f4dfe6483ca622f2b19c4263015395dd148cbdfa
Adam Virostek <adam.virostek@gmail.com>
2023-10-18 16:23:21 +0200

    prepare for firebase SDKs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./Navbar.jsx";
 import Text from "./Text.jsx";
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+// Required for side-effects
+import "firebase/firestore";
 require("dotenv").config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +25,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 function App() {
   return (
