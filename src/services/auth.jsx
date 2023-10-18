e8a9e94eea66abc55ce54f9bb0c80ebab28eaa0d
Adam Virostek <adam.virostek@gmail.com>
2023-10-19 00:21:37 +0200

    prepare for user auth

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,3 +1,36 @@
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+import app from "./firebase";
 
 const auth = getAuth(app);
+
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signOutUser = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    throw error;
+  }
+};
