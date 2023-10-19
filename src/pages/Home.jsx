5de08f4abde795791b3226025702f8dc82483320
Adam Virostek <adam.virostek@gmail.com>
2023-10-19 21:13:28 +0200

    add footer and content to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import Content from "../components/Content.jsx";
+import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Content />
+      <Footer />
     </>
   );
 }
