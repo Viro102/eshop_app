442ec72816d2bab4568afab362ebb14da2d6c3da
Adam Virostek <adam.virostek@gmail.com>
2023-10-21 17:49:58 +0200

    remove flowbite logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,12 @@ export default function Header() {
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 sticky top-0">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to={"/"} className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              E-SHOP
-            </span>
-          </Link>
+          <div className="flex items-center">
+            <Link to={"/"}>
+              <img className="mr-3 h-3 sm:h-9" src="/vite.svg" alt="vite logo" />
+            </Link>
+            <span className="self-center text-xl font-semibold dark:text-white">E-SHOP</span>
+          </div>
 
           <form>
             <label
