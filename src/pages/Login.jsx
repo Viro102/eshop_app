80db502bcd754694e1ad37dec932edc7ee9c0dfd
Adam Virostek <adam.virostek@gmail.com>
2023-10-21 17:49:44 +0200

    change colors and use new react button component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import Button from "../components/Button.jsx";
 
 export default function Login() {
   return (
@@ -12,63 +13,55 @@ export default function Login() {
           <div className="container mx-auto px-4 h-full">
             <div className="flex content-center items-center justify-center h-full">
               <div className="w-full lg:w-4/12 px-4">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-800 border-0">
                   <div className="rounded-t mb-0 px-6 py-6">
                     <div className="text-center mb-3">
-                      <h6 className="text-gray-600 text-sm font-bold">Sign in with</h6>
+                      <h6 className="text-gray-100 text-sm font-bold">Sign in with</h6>
                     </div>
                     <div className="btn-wrapper text-center">
-                      <button
-                        className="bg-white active:bg-gray-100 text-gray-800 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-                        type="button"
-                        style={{ transition: "all .15s ease" }}
-                      >
-                        <img alt="..." className="w-5 mr-1" src="/github.svg" />
-                        Github
-                      </button>
-                      <button
-                        className="bg-white active:bg-gray-100 text-gray-800 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-                        type="button"
-                        style={{ transition: "all .15s ease" }}
-                      >
-                        <img alt="..." className="w-5 mr-1" src="/google.svg" />
-                        Google
-                      </button>
+                      <Button
+                        text={"Github"}
+                        iconSrc={"/github.svg"}
+                        alt={"Sign in using github"}
+                      />
+                      <Button
+                        text={"Google"}
+                        iconSrc={"/google.svg"}
+                        alt={"Sign in using google"}
+                      />
                     </div>
                     <hr className="mt-6 border-b-1 border-gray-400" />
                   </div>
                   <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                    <div className="text-gray-500 text-center mb-3 font-bold">
+                    <div className="text-gray-100 text-center mb-3 font-bold">
                       <small>Or sign in with credentials</small>
                     </div>
                     <form>
                       <div className="relative w-full mb-3">
                         <label
-                          className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                          className="block uppercase text-gray-100 text-xs font-bold mb-2"
                           htmlFor="grid-password"
                         >
                           Email
                         </label>
                         <input
                           type="email"
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                          className="border-0 px-3 py-3 placeholder-gray-400 text-black bg-white rounded text-sm shadow focus:outline-none focus:ring w-full transition-all"
                           placeholder="Email"
-                          style={{ transition: "all .15s ease" }}
                         />
                       </div>
 
                       <div className="relative w-full mb-3">
                         <label
-                          className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                          className="block uppercase text-gray-100 text-xs font-bold mb-2"
                           htmlFor="grid-password"
                         >
                           Password
                         </label>
                         <input
                           type="password"
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                          className="border-0 px-3 py-3 placeholder-gray-400 text-black bg-white rounded text-sm shadow focus:outline-none focus:ring w-full transition-all"
                           placeholder="Password"
-                          style={{ transition: "all .15s ease" }}
                         />
                       </div>
                       <div>
@@ -76,10 +69,10 @@ export default function Login() {
                           <input
                             id="customCheckLogin"
                             type="checkbox"
-                            className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
+                            className="form-checkbox border-0 rounded text-gray-100 ml-1 w-5 h-5"
                             style={{ transition: "all .15s ease" }}
                           />
-                          <span className="ml-2 text-sm font-semibold text-gray-700">
+                          <span className="ml-2 text-sm font-semibold text-gray-100">
                             Remember me
                           </span>
                         </label>
