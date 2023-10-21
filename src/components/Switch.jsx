008bb57daff60eb86f5cf9595b4a21de7fa7fad1
Adam Virostek <adam.virostek@gmail.com>
2023-10-21 17:48:19 +0200

    rework switch component

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,17 +1,9 @@
-import "./SwitchStyle.css";
-
-export default function Switch() {
+export default function Switch({ text }) {
   return (
-    <div className="relative inline-block w-10 h-6 mr-2 align-middle select-none">
-      <input
-        type="checkbox"
-        id="themeToggle"
-        className="toggle-checkbox absolute w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
-      />
-      <label
-        htmlFor="themeToggle"
-        className="toggle-label block w-10 h-6 rounded-full bg-gray-300 cursor-pointer"
-      ></label>
-    </div>
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input type="checkbox" className="hidden peer" />
+      <div className="w-11 h-6 bg-gray-200  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{text}</span>
+    </label>
   );
 }
