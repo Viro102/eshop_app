bdfb8dbdfb242171add5d3f27b692d7e4c5611d8
Adam Virostek <adam.virostek@gmail.com>
2023-10-23 11:12:20 +0200

    test product item on home page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,14 @@
+import ProductItem from "./ProductItem";
+
 export default function Content() {
   return (
-    <main role="main" className="w-full h-full flex-grow p-3 overflow-auto">
+    <main
+      role="main"
+      className="w-full h-full flex-grow p-3 overflow-auto dark:bg-gray-700 bg-gray-200 text-black dark:text-white"
+    >
       BOZO
+      <ProductItem />
+      {/* TODO: Generate products in cycle from db */}
     </main>
   );
 }
