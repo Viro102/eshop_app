cafa08720ca8f56fd91aaa86a6fd1453d7013d14
Adam Virostek <adam.virostek@gmail.com>
2023-10-24 09:36:26 +0200

    add placeholder product cards generator

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,27 @@
+import PropTypes from "prop-types";
 import ProductItem from "./ProductItem";
+
+function ProductList(pocet) {
+  const productItems = [];
+
+  for (let i = 0; i < pocet; i++) {
+    productItems.push(<ProductItem key={i} />);
+  }
+
+  console.log(productItems);
+
+  return <div className="product-list">{productItems}</div>;
+}
 
 export default function Content() {
   return (
-    <main
-      role="main"
-      className="w-full h-full flex-grow p-3 overflow-auto dark:bg-gray-700 bg-gray-200 text-black dark:text-white"
-    >
-      BOZO
-      <ProductItem />
-      {/* TODO: Generate products in cycle from db */}
+    <main className="w-full h-full p-3 overflow-auto dark:bg-gray-700 bg-gray-200 text-black dark:text-white">
+      {ProductList(10)}
+      {/* TODO: Generate products from db */}
     </main>
   );
 }
+
+ProductList.propTypes = {
+  pocet: PropTypes.number,
+};
