2a4745d657d437ade78c8f9e69eeb7299fcf34df
Adam Virostek <adam.virostek@gmail.com>
2023-10-24 09:21:45 +0200

    move theme switch to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import ThemeSwitch from "./ThemeSwitch";
 
 export default function Header() {
   return (
@@ -67,6 +68,9 @@ export default function Header() {
               type="button"
               className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             ></button>
+          </div>
+          <div className="flex items-center">
+            <ThemeSwitch />
           </div>
         </div>
       </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Content from "../components/Content.jsx";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
-import ThemeSwitch from "../components/ThemeSwitch.jsx";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <ThemeSwitch />
       <Content />
       <Footer />
     </>
