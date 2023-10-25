5cb36b97ab17af0126a83a9904a753385df584e1
Adam Virostek <adam.virostek@gmail.com>
2023-10-25 14:59:17 +0200

    style sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Button from "./Button.jsx";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 export default function Sidebar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -10,18 +13,23 @@ export default function Sidebar() {
 
   return (
     <div
-      className={`sidebar fixed left-0 top-0 h-screen w-1/4 bg-gray-900 text-white transition-transform ${
+      className={`sidebar fixed left-0 top-0 h-screen w-1/6 bg-gray-900 text-white transition-transform ${
         sidebarOpen ? "sidebar-open" : "sidebar-close"
       }`}
     >
       <div className="relative left-full">
-        <Button
-          text="Toggle sidebar"
-          onClick={handleToggleSidebar}
-          className="absolute top-2.5 w-20"
-        />
+        <Button onClick={handleToggleSidebar} className="absolute top-4">
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
       </div>
-      <div className="sidebar-content">testing... testing...</div>
+      <div className="sidebar-content">
+        <ul>
+          <li>
+            <Button text={"test"} iconSrc={""}></Button>
+          </li>
+          <li>testing...</li>
+        </ul>
+      </div>
     </div>
   );
 }
