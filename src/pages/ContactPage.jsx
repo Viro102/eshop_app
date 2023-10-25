635ac5c0ad9ef70ed4540a6c64c52b6654d63451
Adam Virostek <adam.virostek@gmail.com>
2023-10-25 22:54:10 +0200

    rework responsive design and use InputForm

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,14 +1,18 @@
+import Button from "../components/Button.jsx";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
+import InputForm from "../components/InputForm.jsx";
 
 export default function ContactPage() {
   return (
     <>
       <Header />
-      <section className="m-32">
+      <section className="m-20">
         <div className="flex justify-center">
           <div className="text-center md:max-w-xl lg:max-w-3xl">
-            <h2 className="mb-12 px-6 text-3xl font-bold">Contact us</h2>
+            <h2 className="mb-12 whitespace-nowrap px-6 py-3 text-3xl font-bold text-gray-900 dark:text-white">
+              Contact us
+            </h2>
           </div>
         </div>
 
@@ -16,70 +20,26 @@ export default function ContactPage() {
           <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
             <form>
               <div className="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  className="peer-focus:text-primary dark:peer-focus:text-primary peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleInput90"
-                  placeholder="Name"
-                />
-                <label
-                  className="peer-focus:text-primary dark:peer-focus:text-primary pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200"
-                  htmlFor="exampleInput90"
-                >
-                  Name
-                </label>
+                <InputForm name={"Name"} type={"text"} placeholder={"Name"} />
               </div>
               <div className="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="email"
-                  className="peer-focus:text-primary dark:peer-focus:text-primary peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleInput91"
-                  placeholder="Email address"
-                />
-                <label
-                  className="peer-focus:text-primary dark:peer-focus:text-primary pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200"
-                  htmlFor="exampleInput91"
-                >
-                  Email address
-                </label>
+                <InputForm name={"Email"} type={"email"} placeholder={"Email"} />
               </div>
-              <div className="relative mb-6" data-te-input-wrapper-init>
-                <textarea
-                  className="border-1 peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                  placeholder="Your message"
-                ></textarea>
-                <label
-                  htmlFor="exampleFormControlTextarea1"
-                  className="peer-focus:text-primary dark:peer-focus:text-primary pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200"
-                >
-                  Message
-                </label>
-              </div>
-              <div className="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
-                <input
-                  className="checked:border-primary checked:bg-primary dark:checked:border-primary dark:checked:bg-primary relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                  type="checkbox"
-                  value=""
-                  id="exampleCheck96"
-                  defaultChecked
-                />
-                <label
-                  className="inline-block pl-[0.15rem] hover:cursor-pointer"
-                  htmlFor="exampleCheck96"
-                >
-                  Send me a copy of this message
-                </label>
-              </div>
-              <button
-                type="button"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-                className="bg-primary hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 mb-6 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+
+              <label
+                htmlFor="message"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
               >
-                Send
-              </button>
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                className="w-full rounded border-0 bg-white px-3 py-3 text-sm text-black placeholder-gray-400 shadow transition-all focus:outline-none focus:ring"
+                placeholder="Write your thoughts here..."
+              />
+              <div className="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex"></div>
+              <Button text={"Send"} className="w-full" />
             </form>
           </div>
           <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
@@ -87,7 +47,7 @@ export default function ContactPage() {
               <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                 <div className="flex items-start">
                   <div className="shrink-0">
-                    <div className="bg-primary-100 text-primary inline-block rounded-md p-4">
+                    <div className="inline-block rounded-md p-4 text-gray-900 dark:text-white">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -105,16 +65,18 @@ export default function ContactPage() {
                     </div>
                   </div>
                   <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">Technical support</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">support@example.com</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">+1 234-567-89</p>
+                    <p className="mb-2 font-bold text-gray-900 dark:text-white">
+                      Technical support
+                    </p>
+                    <p className="text-gray-900 dark:text-white">support@example.com</p>
+                    <p className="text-gray-900 dark:text-white">+1 234-567-89</p>
                   </div>
                 </div>
               </div>
               <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                 <div className="flex items-start">
                   <div className="shrink-0">
-                    <div className="bg-primary-100 text-primary inline-block rounded-md p-4">
+                    <div className="inline-block rounded-md p-4 text-gray-900 dark:text-white">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -132,16 +94,16 @@ export default function ContactPage() {
                     </div>
                   </div>
                   <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">Sales questions</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">sales@example.com</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">+1 234-567-89</p>
+                    <p className="mb-2 font-bold text-gray-900 dark:text-white">Sales questions</p>
+                    <p className="text-gray-900 dark:text-white">sales@example.com</p>
+                    <p className="text-gray-900 dark:text-white">+1 234-567-89</p>
                   </div>
                 </div>
               </div>
               <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                 <div className="align-start flex">
                   <div className="shrink-0">
-                    <div className="bg-primary-100 text-primary inline-block rounded-md p-4">
+                    <div className="inline-block rounded-md p-4 text-gray-900 dark:text-white">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -159,16 +121,16 @@ export default function ContactPage() {
                     </div>
                   </div>
                   <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">Press</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">press@example.com</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">+1 234-567-89</p>
+                    <p className="mb-2 font-bold text-gray-900 dark:text-white">Press</p>
+                    <p className="text-gray-900 dark:text-white">press@example.com</p>
+                    <p className="text-gray-900 dark:text-white">+1 234-567-89</p>
                   </div>
                 </div>
               </div>
               <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                 <div className="align-start flex">
                   <div className="shrink-0">
-                    <div className="bg-primary-100 text-primary inline-block rounded-md p-4">
+                    <div className="inline-block rounded-md p-4 text-gray-900 dark:text-white ">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -186,9 +148,9 @@ export default function ContactPage() {
                     </div>
                   </div>
                   <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">Bug report</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">bugs@example.com</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">+1 234-567-89</p>
+                    <p className="mb-2 font-bold text-gray-900 dark:text-white">Bug report</p>
+                    <p className="text-gray-900 dark:text-white">bugs@example.com</p>
+                    <p className="text-gray-900 dark:text-white">+1 234-567-89</p>
                   </div>
                 </div>
               </div>
