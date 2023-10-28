12196285f1688915847a06493667b7b9af7c554e
Adam Virostek <adam.virostek@gmail.com>
2023-10-28 17:45:55 +0200

    fix font theming

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,10 +4,14 @@ export default function SignUpPage() {
   return (
     <div className="flex justify-center self-center">
       <div className="my-24 flex w-full flex-col rounded-xl px-6 py-3 dark:bg-gray-800 sm:w-6/12 lg:w-4/12">
-        <div className="mb-2 mt-2 flex justify-center whitespace-nowrap font-bold">Sign Up</div>
+        <div className="mb-2 mt-2 flex justify-center whitespace-nowrap font-bold text-gray-900 dark:text-white">
+          Sign Up
+        </div>
         <form>
           <div className="relative mb-3 w-full">
-            <label className="mb-2 block text-xs font-bold uppercase text-gray-100">Email</label>
+            <label className="mb-2 block text-xs font-bold uppercase text-gray-900 dark:text-white">
+              Email
+            </label>
             <input
               type="email"
               className="w-full rounded border-0 bg-white px-3 py-3 text-sm text-black placeholder-gray-400 shadow transition-all focus:outline-none focus:ring"
@@ -16,7 +20,9 @@ export default function SignUpPage() {
           </div>
 
           <div className="relative mb-3 w-full">
-            <label className="mb-2 block text-xs font-bold uppercase text-gray-100">Password</label>
+            <label className="mb-2 block text-xs font-bold uppercase text-gray-900 dark:text-white">
+              Password
+            </label>
             <input
               type="password"
               className="mb-3 w-full rounded border-0 bg-white px-3 py-3 text-sm text-black placeholder-gray-400 shadow transition-all focus:outline-none focus:ring"
