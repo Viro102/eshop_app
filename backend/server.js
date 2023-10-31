64b1a2ea926aa04f9fffa1c0ed5c43cbf09546ea
Adam Virostek <adam.virostek@gmail.com>
2023-10-31 16:31:41 +0100

    add mysql running in docker

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mysql from "mysql2";
 
 const app = express();
 const port = 3000;
@@ -8,5 +9,23 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on localhost:${port}`);
 });
+
+// TODO: .env integeraiton
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "admin",
+  database: "eshop",
+});
+
+connection.connect();
+
+connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log("The solution is: ", rows[0].solution);
+});
+
+connection.end();
