a14c5db9137bc1bf909fd22d863439e864fdef2f
Adam Virostek <adam.virostek@gmail.com>
2023-11-01 15:33:20 +0100

    change file structure

--- backend/src/models/productModel.js
+++
@@ -1,9 +0,0 @@
-export class Product {
-  constructor(title, name, price, category, description) {
-    this.title = title;
-    this.name = name;
-    this.price = price;
-    this.category = category;
-    this.description = description;
-  }
-}
