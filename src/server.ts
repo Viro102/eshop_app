3f976e66fba32230ba1e04e586af29cf1ba6b33e
Adam Virostek <adam.virostek@gmail.com>
2023-11-07 23:01:19 +0100

    add API endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import mysql from "mysql2/promise";
-import { getAllProducts } from "./controllers/productController";
+import { getAllProducts, getProductById } from "./controllers/productController";
 
 const app = express();
 const port = 3000;
@@ -12,9 +12,12 @@ app.get("/bozo", (_req, res) => {
 
 app.get("/products", (_req, res) => {
   console.log("GET /products");
-  getAllProducts().then((products) => {
-    res.send(products);
-  });
+  getAllProducts(_req, res);
+});
+
+app.get("/product/:id", (req, res) => {
+  console.log("GET /product/" + req.params.id);
+  getProductById(req, res);
 });
 
 ViteExpress.listen(app, port, () => {
