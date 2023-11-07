b093ebaff0665774d3a43b990cd6316c28deec0a
Adam Virostek <adam.virostek@gmail.com>
2023-11-07 23:02:18 +0100

    Reorder imports and fix product fetching in Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 import type { Product } from "../models/productModel";
 import ProductItem from "./ProductItem";
 
 const Content = () => {
-  const fetchProducts = async (): Promise<Product[]> => {
-    const response = await fetch("/products");
-    const products: Product[][] = await response.json();
-    console.log(products);
-    return products[0];
-  };
-
   const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = async () => {
+    const response = await fetch("/products");
+    const products: Product[] = await response.json();
+    console.log(products);
+    return products;
+  };
 
   useEffect(() => {
     fetchProducts().then(setProducts);
@@ -25,8 +25,8 @@ const Content = () => {
 };
 
 function ProductList({ products }: Readonly<{ products: Product[] }>) {
-  const productItems = products.map((product, index: number) => (
-    <ProductItem key={index} product={product} />
+  const productItems = products.map((product) => (
+    <ProductItem key={product.id} product={product} />
   ));
 
   return <div className="flex flex-wrap justify-center">{productItems}</div>;
