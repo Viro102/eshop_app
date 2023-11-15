591b996f3fa710f1808a862730520d0650e7718c
Adam Virostek <adam.virostek@gmail.com>
2023-11-15 17:25:44 +0100

    create Cart page components

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,48 @@
+export default function CartItem() {
+  return (
+    <div className="rounded-lg md:w-2/3">
+      <div className="mb-6 justify-between rounded-lg bg-gray-100 p-6 shadow-md dark:bg-gray-800 sm:flex sm:justify-start">
+        <img
+          src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="product-image"
+          className="w-full rounded-lg sm:w-40"
+        />
+        <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+          <div className="mt-5 sm:mt-0">
+            <h2 className="text-lg font-bold ">Nike Air Max 2019</h2>
+            <p className="mt-1 text-xs">36EU - 4US</p>
+          </div>
+          <div className="mt-4 flex justify-between sm:mt-0 sm:block sm:space-x-6 sm:space-y-6">
+            <div className="flex items-center">
+              <span className="h-8 cursor-pointer rounded-l bg-gray-100 px-3.5 py-1 duration-100 hover:bg-blue-500 hover:text-blue-50 dark:bg-gray-700">
+                -
+              </span>
+              <input
+                className="h-8 w-7 bg-white text-center text-xs outline-none dark:bg-gray-700"
+                type="number"
+                defaultValue="1" // TODO!
+                min="1"
+              />
+              <span className="h-8 cursor-pointer rounded-r bg-gray-100 px-3 py-1 duration-100 hover:bg-blue-500 hover:text-blue-50 dark:bg-gray-700">
+                +
+              </span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <p className="text-sm">$259.00</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/CartSummary.tsx
@@ -0,0 +1,29 @@
+import Button from "./Button";
+
+export default function CartSummary() {
+  return (
+    <div className="mt-6 h-full rounded-lg bg-gray-100 p-6  text-gray-900 shadow-md dark:bg-gray-800 dark:text-white md:mt-0 md:w-1/3">
+      <div className="mb-2 flex justify-between">
+        <p>Subtotal</p>
+        <p>$129.99</p>
+      </div>
+      <div className="flex justify-between">
+        <p>Shipping</p>
+        <p>$4.99</p>
+      </div>
+      <hr className="my-4" />
+      <div className="flex justify-between">
+        <p className="text-lg font-bold">Total</p>
+        <div className="">
+          <p className="mb-1 text-lg font-bold">$134.98 USD</p>
+        </div>
+      </div>
+      <Button
+        text="Checkout"
+        alt="Checkout"
+        onClick={() => console.log("clicked checkout")}
+        className="mt-2 w-full justify-center"
+      />
+    </div>
+  );
+}
