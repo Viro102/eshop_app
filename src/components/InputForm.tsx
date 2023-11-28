4faafe218fb56232739e4d0b7c708531a47a7c1e
Adam Virostek <adam.virostek@gmail.com>
2023-11-28 11:34:00 +0100

    implement InputForm component

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,19 +1,26 @@
 type InputFormProps = {
-  name: string;
+  label: string;
+  htmlFor: string;
   type: string;
   placeholder: string;
+  onChange: (e: React.SyntheticEvent) => void;
 };
 
-const InputForm = ({ name, type, placeholder }: InputFormProps) => {
+const InputForm = ({ label, htmlFor, type, placeholder, onChange }: InputFormProps) => {
   return (
     <div className="relative mb-3 w-full">
-      <label className="mb-2 block text-xs font-bold uppercase text-gray-900 dark:text-white">
-        {name}
+      <label
+        htmlFor={htmlFor}
+        className="mb-2 block text-xs font-bold uppercase text-gray-900 dark:text-white"
+      >
+        {label}
       </label>
       <input
         type={type}
+        id={htmlFor}
         className="w-full rounded border-0 bg-white px-3 py-3 text-sm text-black placeholder-gray-400 shadow transition-all focus:outline-none focus:ring"
         placeholder={placeholder}
+        onChange={onChange}
       />
     </div>
   );
