53587d0c8758555f3b1998a6716b88d8a50a9721
Adam Virostek <adam.virostek@gmail.com>
2023-12-03 15:42:44 +0100

    fix typo

--- src/views/AboutPage.tsx
+++ src/views/AboutPage.tsx
@@ -1,5 +1,5 @@
 /* TODO About page */
-export default function AccountPage() {
+export default function AboutPage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">About page</h1>
