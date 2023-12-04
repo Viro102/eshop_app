2ce1670cf4328d8f762bc455824ad6bcf64aa930
Adam Virostek <adam.virostek@gmail.com>
2023-12-04 20:51:24 +0100

    add logout

--- src/views/AccountPage.tsx
+++ src/views/AccountPage.tsx
@@ -1,8 +1,27 @@
-/* TODO Account page with orders, payment, favorites, etc. */
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+
 export default function AccountPage() {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await fetch("/api/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      navigate("/");
+      console.log("Logout successful");
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Account page</h1>
+    <div className="my-5 flex flex-col items-center justify-center">
+      <h2 className="my-4 text-center text-2xl">Account Page</h2>
+      <Button onClick={handleLogout} alt="" text="Log out" />
     </div>
   );
 }
