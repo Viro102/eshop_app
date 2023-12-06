5eb57960cb4943c37ac7f077f0261cd3ede0a754
Adam Virostek <adam.virostek@gmail.com>
2023-12-06 16:35:20 +0100

    use deconstruction to assign values

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -3,32 +3,34 @@ import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import { Product } from "../models/productModel";
 
-const ProductItem = ({ title, rating, price, image_url, id }: Product) => {
+const ProductItem = ({ ...props }: Product) => {
   return (
     <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg bg-white shadow-md dark:bg-gray-800">
       <Link
         className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
-        to={"/product/" + id}
+        to={"/product/" + props.id}
       >
-        <img className="object-cover" src={image_url} />
+        <img className="object-cover" src={props.image_url} />
       </Link>
       <div className="w-full px-5 py-5">
-        <Link to={"/product/" + id}>
-          <h5 className="text-xl tracking-tight text-slate-900 dark:text-white">{title}</h5>
+        <Link to={"/product/" + props.id}>
+          <h5 className="text-xl tracking-tight text-slate-900 dark:text-white">{props.title}</h5>
         </Link>
         <div className="mb-5 mt-2 flex items-center justify-between">
           <p>
-            <span className="text-3xl font-bold text-slate-900 dark:text-white">{price}€</span>
+            <span className="text-3xl font-bold text-slate-900 dark:text-white">
+              {props.price}€
+            </span>
           </p>
           <div className="flex items-center px-1">
-            <Rating count={rating} />
+            <Rating count={props.rating} />
             <span className="ml-3 mr-2 hidden rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold text-black sm:block">
-              {rating}
+              {props.rating}
             </span>
           </div>
         </div>
         <Link
-          to={"/product/" + id}
+          to={"/product/" + props.id}
           className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
         >
           <svg
