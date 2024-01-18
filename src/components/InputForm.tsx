3896b4ef3dbb5389b128b6d7dbf37152c81bc474
Adam Virostek <adam.virostek@gmail.com>
2024-01-18 20:26:59 +0100

    refactor InputForm component to handle children

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,14 +1,28 @@
 type InputFormProps = {
   label: string;
+  name: string;
   htmlFor: string;
   type: string;
   placeholder: string;
-  onChange: (e: React.SyntheticEvent) => void;
+  value: string;
+  className?: string;
+  children?: React.ReactNode;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const InputForm = ({ label, htmlFor, type, placeholder, onChange }: InputFormProps) => {
+const InputForm = ({
+  label,
+  name,
+  htmlFor,
+  type,
+  placeholder,
+  value,
+  className = "",
+  children,
+  onChange,
+}: InputFormProps) => {
   return (
-    <div className="relative mb-3 w-full">
+    <div className="relative w-full">
       <label
         htmlFor={htmlFor}
         className="mb-2 block text-xs font-bold uppercase text-gray-900 dark:text-white"
@@ -16,12 +30,15 @@ const InputForm = ({ label, htmlFor, type, placeholder, onChange }: InputFormPro
         {label}
       </label>
       <input
+        name={name}
         type={type}
         id={htmlFor}
-        className="w-full rounded border-0 bg-white px-3 py-3 text-sm text-black placeholder-gray-400 shadow transition-all focus:outline-none focus:ring"
+        className={`w-full rounded border-0 bg-white px-3 py-3 text-sm text-black placeholder-gray-400 shadow transition-all focus:outline-none focus:ring ${className}`}
         placeholder={placeholder}
         onChange={onChange}
+        value={value}
       />
+      {children}
     </div>
   );
 };
