c3c934f8231961b7592eef8c0462076bc609bc33
Adam Virostek <adam.virostek@gmail.com>
2024-01-18 20:22:06 +0100

    refactor Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,18 @@
 type ButtonProps = {
-  text?: string;
-  iconSrc?: string;
-  alt: string;
-  onClick: (e: React.SyntheticEvent) => void;
   className?: string;
+  type?: "button" | "submit" | "reset";
+  children?: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button = ({ text, iconSrc, alt, onClick, className = "" }: ButtonProps) => {
+const Button = ({ className = "", children, type = "button", onClick }: ButtonProps) => {
   return (
     <button
       className={`inline-flex items-center whitespace-nowrap rounded bg-blue-400 text-center text-xs font-bold uppercase text-gray-100 shadow outline-none transition-all hover:shadow-md focus:outline-none active:bg-gray-600 dark:bg-gray-900 ${className}`}
-      type="button"
+      type={type}
       onClick={onClick}
     >
-      {iconSrc && <img alt={alt} className="mr-1 w-5" src={iconSrc} />}
-      {text}
+      {children}
     </button>
   );
 };
