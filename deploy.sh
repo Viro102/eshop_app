98a854e4cb157eac5455e2ecdb51796530faee35
Adam Virostek <adam.virostek@gmail.com>
2024-01-19 20:46:03 +0100

    WIP deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 # 1. Build the project
-npm run build
+npm run build-frontend
 # 2. Copy the build to the server
-scp -r dist Dockerfile docker nginx.conf server:/home/viro/e-commerce
+scp -r ./frontend/dist ./frontend/Dockerfile ./frontend/nginx.conf server:/home/viro/e-commerce/frontend
+scp -r ./backend/Dockerfile ./backend/src server:/home/viro/e-commerce/backend
+scp -r docker-compose.yml server:/home/viro/e-commerce
 # 3. Restart the server
-ssh server 'cd /home/viro/e-commerce/docker && docker compose up -d'
+ssh server 'cd /home/viro/e-commerce && docker compose up --build -d'
 # 4. Disconnect from the server
 exit
