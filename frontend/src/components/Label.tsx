d4ea80adad28ad30ad3df3b0c60817a292483d59
Adam Virostek <adam.virostek@gmail.com>
2024-01-19 19:40:45 +0100

    refactor to use children

--- frontend/src/components/Label.tsx
+++ frontend/src/components/Label.tsx
@@ -1,16 +1,16 @@
 type LabelProps = {
-  text?: string;
   className?: string;
   htmlFor?: string;
+  children?: React.ReactNode;
 };
 
-const Label = ({ text, className = "", htmlFor }: LabelProps) => {
+const Label = ({ className = "", htmlFor, children }: LabelProps) => {
   return (
     <label
       className={`cursor-pointer rounded bg-blue-400 p-2 font-bold uppercase text-gray-100 shadow outline-none transition-all hover:shadow-md focus:outline-none active:bg-gray-600 dark:bg-gray-900 hover:dark:bg-blue-400 ${className}`}
       htmlFor={htmlFor}
     >
-      {text}
+      {children}
     </label>
   );
 };
