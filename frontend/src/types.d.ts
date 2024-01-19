0184b40a23255dc89dc349e9c674aeffe05faaa4
Adam Virostek <adam.virostek@gmail.com>
2024-01-19 21:19:09 +0100

    add Review type and update AuthContextType

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -17,9 +17,21 @@ interface User {
   updated_at: Date;
 }
 
+interface Review {
+  id?: number;
+  user_id: number;
+  product_id: number;
+  rating: number;
+  comment: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
 interface AuthContextType {
   isLoggedIn: boolean;
   setIsLoggedIn: (loggedIn: boolean) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
 }
 
 interface AuthResponse {
