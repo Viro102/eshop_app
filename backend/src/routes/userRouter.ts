5c0d964af967b5a0fb8d6e75b494c507189d4653
Adam Virostek <adam.virostek@gmail.com>
2024-01-20 17:30:36 +0100

    add getUserById API endpoint

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { loginUser, logoutUser, signUpUser, statusUser } from "../controllers/userController";
+import { loginUser, getUserById, signUpUser, statusUser } from "../controllers/userController";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
-router.post("/logout", logoutUser);
 router.post("/sign-up", signUpUser);
+router.get("/:id", getUserById);
 router.get("/", statusUser);
 
 export default router;
