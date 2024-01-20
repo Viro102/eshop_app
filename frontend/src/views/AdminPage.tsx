0d04dadc855d8fe93cf1f7125e49b1402229707d
Adam Virostek <adam.virostek@gmail.com>
2024-01-20 22:25:01 +0100

    add tabs to admin page

--- frontend/src/views/AdminPage.tsx
+++ frontend/src/views/AdminPage.tsx
@@ -1,152 +1,49 @@
-import { useEffect, useState } from "react";
-import { deleteProduct, fetchProducts, patchProduct, postProduct } from "../api";
-import Button from "../components/Button";
-import InputForm from "../components/InputForm";
-import ListItem from "../components/ListItem";
+import { useState } from "react";
+import ProductManagement from "../components/ProductManagement";
+import UserManagement from "../components/UserManagement";
 
 export default function AdminPage() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [product, setProduct] = useState<Product>({
-    id: 0,
-    title: "",
-    category: "",
-    image_url: "",
-    price: 0,
-    description: "",
-    rating: 0,
-  });
-
-  useEffect(() => {
-    fetchProducts().then((data) => setProducts(data));
-  }, []);
-
-  const refreshForm = () => {
-    setProduct({
-      id: 0,
-      title: "",
-      category: "",
-      image_url: "",
-      price: 0,
-      description: "",
-      rating: 0,
-    });
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-
-    setProduct({ ...product, [event.target.name]: event.target.value });
-  };
-
-  const handleCreateProduct = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    try {
-      await postProduct(product);
-      await fetchProducts();
-      refreshForm();
-    } catch (error) {
-      alert("Error: " + error);
-    }
-  };
-
-  const handleUpdateProduct = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    try {
-      await patchProduct(product.id, product);
-      await fetchProducts();
-      refreshForm();
-    } catch (error) {
-      alert("Error: " + error);
-    }
-  };
-
-  const handleDeleteProduct = async (productID: number) => {
-    try {
-      await deleteProduct(productID);
-      await fetchProducts();
-      refreshForm();
-    } catch (error) {
-      alert("Error: " + error);
-    }
-  };
+  const [activeTab, setActiveTab] = useState<"products" | "orders" | "users" | "reviews">(
+    "products",
+  );
 
   return (
     <section>
       <h1 className="my-4 text-center text-3xl font-bold">Admin page</h1>
-      <div className="mx-auto my-5 flex w-4/5 flex-col items-center justify-center self-center">
-        <Button className="my-2" onClick={handleCreateProduct}>
-          Add product
-        </Button>
-        <ul>
-          {products.map((productItem) => (
-            <ListItem
-              key={productItem.id}
-              product={productItem}
-              onClick={() => setProduct(productItem)}
-              onDelete={() => handleDeleteProduct(productItem.id)}
-            />
-          ))}
-        </ul>
-        <form>
-          <InputForm
-            name="title"
-            label="Title"
-            htmlFor="title"
-            type="text"
-            placeholder="Enter title"
-            onChange={handleInputChange}
-            value={product.title}
-          />
-          <InputForm
-            name="category"
-            label="Category"
-            htmlFor="category"
-            type="text"
-            placeholder="Enter category"
-            onChange={handleInputChange}
-            value={product.category}
-          />
-          <InputForm
-            name="image_url"
-            label="Image URL"
-            htmlFor="image_url"
-            type="text"
-            placeholder="Enter image URL"
-            onChange={handleInputChange}
-            value={product.image_url}
-          />
-          <InputForm
-            name="price"
-            label="Price"
-            htmlFor="price"
-            type="number"
-            placeholder="Enter price"
-            onChange={handleInputChange}
-            value={product.price.toString()}
-          />
-          <InputForm
-            name="description"
-            label="Description"
-            htmlFor="description"
-            type="text"
-            placeholder="Enter description"
-            onChange={handleInputChange}
-            value={product.description}
-          />
-          <InputForm
-            name="rating"
-            label="Rating"
-            htmlFor="rating"
-            type="number"
-            placeholder="Enter rating"
-            onChange={handleInputChange}
-            value={product.rating.toString()}
-          />
+      <div className="flex justify-center gap-4 p-4">
+        <button
+          onClick={() => setActiveTab("products")}
+          className={activeTab === "products" ? "font-bold" : ""}
+        >
+          Products
+        </button>
+        <button
+          onClick={() => setActiveTab("users")}
+          className={activeTab === "users" ? "font-bold" : ""}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => setActiveTab("reviews")}
+          className={activeTab === "reviews" ? "font-bold" : ""}
+        >
+          Reviews
+        </button>
+      </div>
+      <div className="mx-auto my-5 w-4/5 self-center">
+        {activeTab === "products" && (
+          <div>
+            <ProductManagement />
+          </div>
+        )}
 
-          <Button onClick={handleUpdateProduct}>Update Product</Button>
-        </form>
+        {activeTab === "users" && (
+          <div>
+            <UserManagement />
+          </div>
+        )}
+
+        {activeTab === "reviews" && <div>{/* TODO: reviews management */}</div>}
       </div>
     </section>
   );
