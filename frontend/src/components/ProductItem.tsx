3413fce5e1079954edb24bf3b7c6767eb8bb6114
Adam Virostek <adam.virostek@gmail.com>
2024-01-20 17:49:40 +0100

    change css styling

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -5,24 +5,21 @@ import Button from "./Button";
 
 const ProductItem = ({ ...props }: Product) => {
   return (
-    <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg bg-white shadow-md dark:bg-gray-800">
-      <Link
-        className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
-        to={"/products/" + props.id}
-      >
+    <div className="m-10 flex w-full max-w-xs flex-col rounded-lg bg-white shadow-md dark:bg-gray-800">
+      <Link className="m-3 flex h-60 rounded-xl" to={"/products/" + props.id}>
         <img className="object-cover" src={props.image_url} alt="" />
       </Link>
-      <div className="w-full px-5 py-5">
+      <div className="bottom-0 w-full p-4">
         <Link to={"/products/" + props.id}>
           <h5 className="text-xl tracking-tight text-slate-900 dark:text-white">{props.title}</h5>
         </Link>
-        <div className="mb-5 mt-2 flex items-center justify-between">
+        <div className="flex justify-between pb-5 pt-2">
           <p>
             <span className="text-3xl font-bold text-slate-900 dark:text-white">
               {props.price}€
             </span>
           </p>
-          <div className="flex items-center px-1">
+          <div className="flex items-center">
             <Rating count={props.rating ?? 0} />
             <span className="ml-3 mr-2 hidden rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold text-black sm:block">
               {props.rating}
